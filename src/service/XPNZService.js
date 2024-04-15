559d85623ca147ef534266343bde6e2501ae0355
Tom Ginsberg <tom@beit.tech>
2024-04-15 18:07:32 -0400

    updated client lib

--- src/service/XPNZService.js
+++ src/service/XPNZService.js
@@ -4,8 +4,8 @@ import axios from "axios";
 const endpoint = "http://titanium:3000/api/";
 
 export const XPNZService = {
-  getTransactions(workspaceId) {
-    // get from endpoint /transactions with key workspaceId
+  getTransactions(ledgerId) {
+    // get from endpoint /transactions with key ledgerId
     // {
     //   price: 66.6 (int or float),
     //   category: "expense category" (string),
@@ -25,52 +25,55 @@ export const XPNZService = {
     //     endDate: 1546318800 (unix timestamp)
     //   }
     // }
+    // results should be sorted by date
     return axios.get(endpoint + "transactions", {
       params: {
-        workspaceId: workspaceId
+        "ledger_id": ledgerId
       }
     }).then(response => {
       return response.data;
     });
   },
-  getActiveMembers(workspaceId) {
-    // get from endpoint /members with key workspaceId
+  getActiveMembers(ledgerId) {
+    // get from endpoint /members with key ledgerId
     // [flat list of names (string)]
-    return axios.get(endpoint + "activeMembers", {
+    return axios.get(endpoint + "members", {
       params: {
-        workspaceId: workspaceId
+        "ledger_id": ledgerId,
+        "active": true
       }
     }).then(response => {
       return response.data;
     });
   },
-  getAllMembers(workspaceId) {
-    return axios.get(endpoint + "allMembers", {
+  getAllMembers(ledgerId) {
+    return axios.get(endpoint + "members", {
       params: {
-        workspaceId: workspaceId
+        "ledger_id": ledgerId
       }
     }).then(response => {
       return response.data;
     });
   },
-  getBalances(workspaceId) {
-    // get from endpoint /balances with key workspaceId
+  getBalances(ledgerId) {
+    // get from endpoint /balances with key ledgerId
     // {
     //   memberName: balance (int or float)
     // }
-    return axios.get(endpoint + "balances", {
+    return axios.get(endpoint + "members", {
       params: {
-        workspaceId: workspaceId
+        "ledger_id": ledgerId,
+        "balances": true
       }
     }).then(response => {
       return response.data;
     });
   },
-  getCategories(workspaceId) {
-    // get from endpoint /categories with key workspaceId
+  getCategories(ledgerId) {
+    // get from endpoint /categories with key ledgerId
     return axios.get(endpoint + "categories", {
       params: {
-        workspaceId: workspaceId
+        "ledger_id": ledgerId
       }
     }).then(response => {
       return response.data;
@@ -82,39 +85,50 @@ export const XPNZService = {
       return response.data;
     });
   },
-  addTransaction(workspaceId, transaction) {
-    // post to endpoint /transactions with key workspaceId
+  addTransaction(ledgerId, transaction) {
+    // post to endpoint /transactions with key ledgerId
     // transaction is the same as the response from getTransactions
     // but without the id field
-    return axios.post(endpoint + "saveTransaction", {
-      workspaceId: workspaceId,
+    return axios.post(endpoint + "transactions", {
+      "ledger_id": ledgerId,
       transaction: transaction
     }).then(response => {
       return response.data;
     });
   },
-  addMember(workspaceId, member) {
-    // post to endpoint /members with key workspaceId
-    return axios.post(endpoint + "addMembers", {
-      workspaceId: workspaceId,
-      member: member
+  deleteTransaction(ledgerId, transactionId) {
+    // post to endpoint /transactions with key ledgerId
+    return axios.post(endpoint + "transactions", {
+      "ledger_id": ledgerId,
+      transactionId: transactionId
     }).then(response => {
       return response.data;
     });
   },
-  archiveMember(workspaceId, member) {
-    // post to endpoint /archiveMember with key workspaceId
-    return axios.post(endpoint + "archiveMember", {
-      workspaceId: workspaceId,
-      member: member
+  addMember(ledgerId, member) {
+    // post to endpoint /members with key ledgerId
+    return axios.post(endpoint + "members", {
+      "ledger_id": ledgerId,
+      member: member,
+      action: "add"
     }).then(response => {
       return response.data;
     });
   },
-  settleDebt(workspaceId, fromMember, toMember, amount) {
-    // post to endpoint /settleDebt with key workspaceId
-    return axios.post(endpoint + "settleDebt", {
-      workspaceId: workspaceId,
+  archiveMember(ledgerId, member) {
+    // post to endpoint /members with key ledgerId
+    return axios.post(endpoint + "members", {
+      "ledger_id": ledgerId,
+      member: member,
+      action: "archive"
+    }).then(response => {
+      return response.data;
+    });
+  },
+  settleDebt(ledgerId, fromMember, toMember, amount) {
+    // post to endpoint /settleDebt with key ledgerId
+    return axios.post(endpoint + "settle", {
+      "ledger_id": ledgerId,
       fromMember: fromMember,
       toMember: toMember,
       amount: amount
