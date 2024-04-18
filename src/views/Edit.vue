debe20134b481e41b1b57403336676e971a62da4
Tom Ginsberg <tom@beit.tech>
2024-04-17 23:13:15 -0400

    currency dropdown

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -31,6 +31,22 @@ import Dialog from "primevue/dialog";
 
 const date = ref(null);
 
+const flagEmoji = {
+  CAD: "🇨🇦",
+  USD: "🇺🇸",
+  EUR: "🇪🇺",
+};
+const currencies = ["CAD", "USD", "EUR"];
+const showDropdown = ref(false);
+function toggleDropdown() {
+  showDropdown.value = !showDropdown.value;
+}
+
+function selectCurrency(currency) {
+  product.value.currency = currency;
+  showDropdown.value = false;
+}
+
 const weights = ref([]);
 onMounted(async () => {
   initFlowbite();
@@ -259,58 +275,16 @@ watch(totalContributions, (newValue) => {
           <i class="pi pi-lock text-xl text-green-500"></i>
         </div>
       </div>
+
       <button
+        @click="toggleDropdown"
         id="dropdown-currency-button"
-        data-dropdown-toggle="dropdown-currency"
-        class="z-4 inline-flex flex-shrink-0 items-center rounded-e-lg border border-gray-300 bg-gray-100 px-4 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+        class="z-[6] inline-flex flex-shrink-0 items-center rounded-e-lg border border-gray-300 bg-gray-100 px-4 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-700"
         type="button"
       >
-        <svg
-          fill="none"
-          aria-hidden="true"
-          class="me-2 h-4 w-4"
-          viewBox="0 0 20 15"
-        >
-          <rect
-            width="19.1"
-            height="13.5"
-            x=".25"
-            y=".75"
-            fill="#fff"
-            stroke="#F5F5F5"
-            stroke-width=".5"
-            rx="1.75"
-          />
-          <mask
-            id="a"
-            style="mask-type: luminance"
-            width="20"
-            height="15"
-            x="0"
-            y="0"
-            maskUnits="userSpaceOnUse"
-          >
-            <rect
-              width="19.1"
-              height="13.5"
-              x=".25"
-              y=".75"
-              fill="#fff"
-              stroke="#fff"
-              stroke-width=".5"
-              rx="1.75"
-            />
-          </mask>
-          <g fill="#FF3131" mask="url(#a)">
-            <path d="M14 .5h5.6v14H14z" />
-            <path
-              fill-rule="evenodd"
-              d="M0 14.5h5.6V.5H0v14zM11.45 6.784a.307.307 0 01-.518-.277l.268-1.34-.933.466-.467-1.4-.467 1.4-.933-.466.268 1.34a.307.307 0 01-.518.277.307.307 0 00-.434 0l-.25.25-.933-.467L7 7.5l-.231.231a.333.333 0 000 .471l1.164 1.165h1.4l.234 1.4h.466l.234-1.4h1.4l1.164-1.165a.333.333 0 000-.471l-.231-.23.467-.934-.934.466-.25-.25a.307.307 0 00-.433 0z"
-              clip-rule="evenodd"
-            />
-          </g>
-        </svg>
-        CAD
+        <span class="mr-2">{{ flagEmoji[product.currency] }}</span>
+
+        {{ product.currency }}
         <svg
           class="ms-2.5 h-2.5 w-2.5"
           aria-hidden="true"
@@ -329,193 +303,29 @@ watch(totalContributions, (newValue) => {
       </button>
       <div
         id="dropdown-currency"
-        class="z-[6] hidden w-36 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
+        class="z-[6] w-36 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
+        :class="[showDropdown ? 'block' : 'hidden']"
+        style="
+          position: absolute;
+          inset: 0px auto auto 0px;
+          margin: 0px;
+          transform: translate3d(267.429px, 222.095px, 0px);
+        "
+        data-popper-placement="bottom"
       >
         <ul
           class="py-2 text-sm text-gray-700 dark:text-gray-200"
           aria-labelledby="dropdown-currency-button"
         >
-          <li>
+          <li v-for="currency in currencies">
             <button
+              @click="selectCurrency(currency)"
               type="button"
               class="inline-flex w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
               role="menuitem"
             >
               <div class="inline-flex items-center">
-                <svg
-                  fill="none"
-                  aria-hidden="true"
-                  class="me-2 h-4 w-4"
-                  viewBox="0 0 20 15"
-                >
-                  <rect
-                    width="19.1"
-                    height="13.5"
-                    x=".25"
-                    y=".75"
-                    fill="#fff"
-                    stroke="#F5F5F5"
-                    stroke-width=".5"
-                    rx="1.75"
-                  />
-                  <mask
-                    id="a"
-                    style="mask-type: luminance"
-                    width="20"
-                    height="15"
-                    x="0"
-                    y="0"
-                    maskUnits="userSpaceOnUse"
-                  >
-                    <rect
-                      width="19.1"
-                      height="13.5"
-                      x=".25"
-                      y=".75"
-                      fill="#fff"
-                      stroke="#fff"
-                      stroke-width=".5"
-                      rx="1.75"
-                    />
-                  </mask>
-                  <g fill="#FF3131" mask="url(#a)">
-                    <path d="M14 .5h5.6v14H14z" />
-                    <path
-                      fill-rule="evenodd"
-                      d="M0 14.5h5.6V.5H0v14zM11.45 6.784a.307.307 0 01-.518-.277l.268-1.34-.933.466-.467-1.4-.467 1.4-.933-.466.268 1.34a.307.307 0 01-.518.277.307.307 0 00-.434 0l-.25.25-.933-.467L7 7.5l-.231.231a.333.333 0 000 .471l1.164 1.165h1.4l.234 1.4h.466l.234-1.4h1.4l1.164-1.165a.333.333 0 000-.471l-.231-.23.467-.934-.934.466-.25-.25a.307.307 0 00-.433 0z"
-                      clip-rule="evenodd"
-                    />
-                  </g>
-                </svg>
-                CAD
-              </div>
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              class="inline-flex w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-              role="menuitem"
-            >
-              <div class="inline-flex items-center">
-                <svg
-                  fill="none"
-                  aria-hidden="true"
-                  class="me-2 h-4 w-4"
-                  viewBox="0 0 20 15"
-                >
-                  <rect width="19.6" height="14" y=".5" fill="#fff" rx="2" />
-                  <mask
-                    id="a"
-                    style="mask-type: luminance"
-                    width="20"
-                    height="15"
-                    x="0"
-                    y="0"
-                    maskUnits="userSpaceOnUse"
-                  >
-                    <rect width="19.6" height="14" y=".5" fill="#fff" rx="2" />
-                  </mask>
-                  <g mask="url(#a)">
-                    <path
-                      fill="#D02F44"
-                      fill-rule="evenodd"
-                      d="M19.6.5H0v.933h19.6V.5zm0 1.867H0V3.3h19.6v-.933zM0 4.233h19.6v.934H0v-.934zM19.6 6.1H0v.933h19.6V6.1zM0 7.967h19.6V8.9H0v-.933zm19.6 1.866H0v.934h19.6v-.934zM0 11.7h19.6v.933H0V11.7zm19.6 1.867H0v.933h19.6v-.933z"
-                      clip-rule="evenodd"
-                    />
-                    <path fill="#46467F" d="M0 .5h8.4v6.533H0z" />
-                    <g filter="url(#filter0_d_343_121520)">
-                      <path
-                        fill="url(#paint0_linear_343_121520)"
-                        fill-rule="evenodd"
-                        d="M1.867 1.9a.467.467 0 11-.934 0 .467.467 0 01.934 0zm1.866 0a.467.467 0 11-.933 0 .467.467 0 01.933 0zm1.4.467a.467.467 0 100-.934.467.467 0 000 .934zM7.467 1.9a.467.467 0 11-.934 0 .467.467 0 01.934 0zM2.333 3.3a.467.467 0 100-.933.467.467 0 000 .933zm2.334-.467a.467.467 0 11-.934 0 .467.467 0 01.934 0zm1.4.467a.467.467 0 100-.933.467.467 0 000 .933zm1.4.467a.467.467 0 11-.934 0 .467.467 0 01.934 0zm-2.334.466a.467.467 0 100-.933.467.467 0 000 .933zm-1.4-.466a.467.467 0 11-.933 0 .467.467 0 01.933 0zM1.4 4.233a.467.467 0 100-.933.467.467 0 000 .933zm1.4.467a.467.467 0 11-.933 0 .467.467 0 01.933 0zm1.4.467a.467.467 0 100-.934.467.467 0 000 .934zM6.533 4.7a.467.467 0 11-.933 0 .467.467 0 01.933 0zM7 6.1a.467.467 0 100-.933.467.467 0 000 .933zm-1.4-.467a.467.467 0 11-.933 0 .467.467 0 01.933 0zM3.267 6.1a.467.467 0 100-.933.467.467 0 000 .933zm-1.4-.467a.467.467 0 11-.934 0 .467.467 0 01.934 0z"
-                        clip-rule="evenodd"
-                      />
-                    </g>
-                  </g>
-                  <defs>
-                    <linearGradient
-                      id="paint0_linear_343_121520"
-                      x1=".933"
-                      x2=".933"
-                      y1="1.433"
-                      y2="6.1"
-                      gradientUnits="userSpaceOnUse"
-                    >
-                      <stop stop-color="#fff" />
-                      <stop offset="1" stop-color="#F0F0F0" />
-                    </linearGradient>
-                    <filter
-                      id="filter0_d_343_121520"
-                      width="6.533"
-                      height="5.667"
-                      x=".933"
-                      y="1.433"
-                      color-interpolation-filters="sRGB"
-                      filterUnits="userSpaceOnUse"
-                    >
-                      <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                      <feColorMatrix
-                        in="SourceAlpha"
-                        result="hardAlpha"
-                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                      />
-                      <feOffset dy="1" />
-                      <feColorMatrix
-                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.06 0"
-                      />
-                      <feBlend
-                        in2="BackgroundImageFix"
-                        result="effect1_dropShadow_343_121520"
-                      />
-                      <feBlend
-                        in="SourceGraphic"
-                        in2="effect1_dropShadow_343_121520"
-                        result="shape"
-                      />
-                    </filter>
-                  </defs>
-                </svg>
-                USD
-              </div>
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              class="inline-flex w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-              role="menuitem"
-            >
-              <div class="inline-flex items-center">
-                <svg
-                  fill="none"
-                  aria-hidden="true"
-                  class="me-2 h-4 w-4"
-                  viewBox="0 0 20 15"
-                >
-                  <rect width="19.6" height="14" y=".5" fill="#fff" rx="2" />
-                  <mask
-                    id="a"
-                    style="mask-type: luminance"
-                    width="20"
-                    height="15"
-                    x="0"
-                    y="0"
-                    maskUnits="userSpaceOnUse"
-                  >
-                    <rect width="19.6" height="14" y=".5" fill="#fff" rx="2" />
-                  </mask>
-                  <g mask="url(#a)">
-                    <path fill="#043CAE" d="M0 .5h19.6v14H0z" />
-                    <path
-                      fill="#FFD429"
-                      fill-rule="evenodd"
-                      d="M9.14 3.493L9.8 3.3l.66.193-.193-.66.193-.66-.66.194-.66-.194.193.66-.193.66zm0 9.334l.66-.194.66.194-.193-.66.193-.66-.66.193-.66-.193.193.66-.193.66zm5.327-4.86l-.66.193L14 7.5l-.193-.66.66.193.66-.193-.194.66.194.66-.66-.193zm-9.994.193l.66-.193.66.193L5.6 7.5l.193-.66-.66.193-.66-.193.194.66-.194.66zm9.369-2.527l-.66.194.193-.66-.194-.66.66.193.66-.193-.193.66.193.66-.66-.194zm-8.743 4.86l.66-.193.66.193-.194-.66.194-.66-.66.194-.66-.194.193.66-.193.66zm7.034-6.568l-.66.193.194-.66-.194-.66.66.194.66-.193-.193.66.193.66-.66-.194zm-5.326 8.276l.66-.193.66.193-.194-.66.194-.66-.66.194-.66-.193.193.66-.193.66zM13.84 10.3l-.66.193.194-.66-.194-.66.66.194.66-.194-.193.66.193.66-.66-.193zM5.1 5.827l.66-.194.66.194-.194-.66.194-.66-.66.193-.66-.193.193.66-.193.66zm7.034 6.181l-.66.193.194-.66-.194-.66.66.194.66-.193-.193.66.193.66-.66-.194zm-5.326-7.89l.66-.193.66.193-.194-.66.194-.66-.66.194-.66-.193.193.66-.193.66z"
-                      clip-rule="evenodd"
-                    />
-                  </g>
-                </svg>
-                EUR
+                {{ flagEmoji[currency] }} {{ currency }}
               </div>
             </button>
           </li>
@@ -660,7 +470,9 @@ watch(totalContributions, (newValue) => {
       v-if="unequalSplit"
       class="mt-5 rounded-lg border border-gray-300 px-2 py-3 dark:border-gray-600"
     >
-      <p class="text-sm font-semibold text-white">Split Weights</p>
+      <p class="text-sm font-semibold text-gray-900 dark:text-white">
+        Split Weights
+      </p>
       <div class="max-w flex flex-wrap pt-2">
         <div v-for="(member, index) in product.for" class="flex flex-col px-1">
           <label
@@ -828,3 +640,12 @@ watch(totalContributions, (newValue) => {
     </Dialog>
   </div>
 </template>
+
+<style scoped>
+.dropdown-hidden {
+  display: none;
+}
+.dropdown-visible {
+  display: block;
+}
+</style>
