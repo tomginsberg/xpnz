53704527626ed9abdd5bd0147bf05b4d790256f0
Tom Ginsberg <tom@beit.tech>
2024-09-03 11:44:06 -0400

    added back number edit

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -713,6 +713,7 @@ function copyExpense() {
           </label>
 
             <input
+              type="number"
               v-model="forWeights[index]"
               class="block w-20 flex-1 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
               inputmode="none"
